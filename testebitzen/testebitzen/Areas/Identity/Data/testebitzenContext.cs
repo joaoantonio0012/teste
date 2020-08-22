8811f0ded0fe321d0d198d5c007b7c3156956a57
cyberlacs <cyberlacs@gmail.com>
2020-08-22 16:59:22 -0300

    Criar Projeto

---
+++ testebitzen/testebitzen/Areas/Identity/Data/testebitzenContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using testebitzen.Areas.Identity.Data;
+
+namespace testebitzen.Data
+{
+    public class testebitzenContext : IdentityDbContext<testebitzenUser>
+    {
+        public testebitzenContext(DbContextOptions<testebitzenContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ testebitzen/testebitzen/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using testebitzen.Areas.Identity.Data;
+using testebitzen.Data;
+
+[assembly: HostingStartup(typeof(testebitzen.Areas.Identity.IdentityHostingStartup))]
+namespace testebitzen.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<testebitzenContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("testebitzenContextConnection")));
+
+                services.AddDefaultIdentity<testebitzenUser>()
+                    .AddEntityFrameworkStores<testebitzenContext>();
+            });
+        }
+    }
+}
